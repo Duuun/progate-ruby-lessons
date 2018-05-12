1fb9f224e2ab00ea8da6809f5951135942c94bf2
Daisuke Murakami <daimura10@gmail.com>
2018-05-12 23:49:21 +0900

    added lesson1

---
+++ lesson1/hello.rb
@@ -0,0 +1,25 @@
+# 変数
+# msg = "hello world"
+# puts msg
+
+# 定数
+# VERSION = 1.1
+# puts VERSION
+
+# VERSION = 1.2
+# puts VERSION
+
+# name = "murakami"
+
+# puts name.empty?
+# puts name.include?("mura")
+
+# 配列
+# colors = ["red", "blue", "yellow"]
+# puts colors[0]
+# puts colors[0..2]
+
+# ハッシュ
+# scores = {"taguchi" => 200, "fcoji" => 400}
+# scores = {:taguchi => 200, :fcoji => 400}
+scores = {taguchi => 200, fcoji => 400}
