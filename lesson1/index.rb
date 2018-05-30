d825a73c5f94102821b36e9fb31f6ebc156331f1
Daisuke Murakami <daimura10@gmail.com>
2018-05-30 23:43:41 +0900

    lesson1-1

---
+++ lesson1/index.rb
@@ -0,0 +1,32 @@
+#文字列連結
+puts "私は" + "にんじゃわんこです"
+puts "38" + "19"
+
+#変数
+name = "にんじゃわんこ"
+puts name
+
+text = "プログラミングを学ぼう"
+puts "Progateで" + text
+
+length = 8
+width = 9
+puts lenght * width
+
+text = "をマスターしよう"
+puts "HTML" + text
+puts "CSS" + text
+puts "Ruby" + text
+
+#変数展開
+length = 9
+width = 8
+area = length * width
+puts "面積は#{area}です"
+
+#条件分岐
+score = 90
+if score > 80
+  puts "よくできました"
+end
+
