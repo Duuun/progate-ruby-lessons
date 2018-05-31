38766c31ea60e2bc082220f136a953fa6e3ec67c
Daisuke Murakami <daimura10@gmail.com>
2018-06-01 01:09:36 +0900

    updated

--- lesson3/index.rb
+++ lesson3/index.rb
@@ -7,3 +7,27 @@ end
 print_info("ヘッドホン")
 print_info("テレビ")
 
+# 複数の引数
+def introduce(name, age)
+  puts "私は#{name}です"
+  puts "#{age}歳です"
+end
+
+# 戻り値
+def discount(price)
+  return price / 2
+end
+
+half_price = discount(15000)
+puts "特別価格で#{half_price}円です"
+
+# 戻り値 真偽値
+def shopping_free?(price)
+  return price >= 5000
+end
+
+if shopping_free?(3000)
+  puts "5000以上のお買い上げなので送料はいただきません"
+else
+  puts "追加で送料をいただきます"
+end
